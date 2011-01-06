eb72c17d591e0cad86e4fc4bc54ec29569dcf8f5
Dinnerbone <dinnerbone@dinnerbone.com>
2011-01-06 16:09:38 +0000

    Fixed /tphere with no args, added red colour to error messages

--- src/main/java/com/dinnerbone/bukkit/scrap/ScrapPlayerListener.java
+++ src/main/java/com/dinnerbone/bukkit/scrap/ScrapPlayerListener.java
@@ -2,7 +2,6 @@
 package com.dinnerbone.bukkit.scrap;
 
 import org.bukkit.Color;
-import org.bukkit.Location;
 import org.bukkit.Player;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerListener;
@@ -23,8 +22,13 @@ public class ScrapPlayerListener extends PlayerListener {
     public void onPlayerCommand(PlayerChatEvent event) {
         String[] split = event.getMessage().split(" ");
         Player player = event.getPlayer();
+        final String command = split[0];
 
-        if ((!event.isCancelled()) && (split[0].equalsIgnoreCase("/tp"))) {
+        if (event.isCancelled()) {
+            return;
+        }
+
+        if (command.equalsIgnoreCase("/tp")) {
             if (split.length == 2) {
                 String dest = split[1];
                 
@@ -50,19 +54,25 @@ public class ScrapPlayerListener extends PlayerListener {
                     }
                 }
             } else {
-                player.sendMessage("Incorrect usage of command /tp. Examples:");
-                player.sendMessage("/tp Dinnerbone - teleports you to the player named Dinnerbone");
-                player.sendMessage("/tp Bukkit Walrus - teleports a player named Bukkit to a player named Walrus");
-                player.sendMessage("/tp * Monster - telports every online player to a player named Monster");
+                player.sendMessage(Color.RED + "Incorrect usage of command /tp. Examples:");
+                player.sendMessage(Color.RED + "/tp Dinnerbone - teleports you to the player named Dinnerbone");
+                player.sendMessage(Color.RED + "/tp Bukkit Walrus - teleports a player named Bukkit to a player named Walrus");
+                player.sendMessage(Color.RED + "/tp * Monster - telports every online player to a player named Monster");
             }
 
             event.setCancelled(true);
-        } else if ((!event.isCancelled()) && (split[0].equalsIgnoreCase("/tphere"))) {
-            String victim = split[1];
+        } else if (command.equalsIgnoreCase("/tphere")) {
+            if (split.length == 2) {
+                String victim = split[1];
 
-            if (!plugin.teleport(victim, player)) {
-                player.sendMessage(Color.RED + "Could not teleport " + victim
-                        + " to you (Is the name spelt correctly?)");
+                if (!plugin.teleport(victim, player)) {
+                    player.sendMessage(Color.RED + "Could not teleport " + victim
+                            + " to you (Is the name spelt correctly?)");
+                }
+            } else {
+                player.sendMessage(Color.RED + "Incorrect usage of command /tphere. Examples:");
+                player.sendMessage(Color.RED + "/tphere Dinnerbone - teleports the player named Dinnerbone to you");
+                player.sendMessage(Color.RED + "/tphere * - teleports every online player to yourself");
             }
 
             event.setCancelled(true);
